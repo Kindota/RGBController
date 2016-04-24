991a42b2f4b67ebe3bf4b5c95cf599c484d8e77f
David Michael Weis <dweis7@gmail.com>
2016-04-24 03:58:05 +0200

    Created RGB Interface

---
+++ C#.NET/RGBController/RGBControllerCommunicator/ArduinoRGBController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace RGBControllerCommunicator
+{
+    public class ArduinoRGBController : RGBComponentInterface
+    {
+        private SerialPort _SerialPort;
+
+        public int cycles
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public float resolution
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void setColor(float red, float green, float blue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setCycle(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ C#.NET/RGBController/RGBControllerCommunicator/RGBComponentInterface.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGBControllerCommunicator
+{
+    interface RGBComponentInterface
+    {
+        int cycles
+        {
+            get;
+        }
+
+        float resolution
+        {
+            get;
+        }
+
+        void setColor(float red, float green, float blue);
+        void setCycle(int number);
+    }
+}
